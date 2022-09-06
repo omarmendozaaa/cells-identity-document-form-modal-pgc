545e14e7345a385b5aff83c1190c893949aa24d6
omar.mendoza.contractor <omar.mendoza.contractor@bbva.com>
2022-09-06 12:17:27 -0500

    aber

--- src/CellsIdentityDocumentFormModalPgc.js
+++ src/CellsIdentityDocumentFormModalPgc.js
@@ -5,8 +5,12 @@ import '@bbva-web-components/bbva-web-button-default/bbva-web-button-default.js'
 import '@bbva-web-components/bbva-web-form-select/bbva-web-form-select.js';
 import '@bbva-web-components/bbva-web-form-select/bbva-web-form-option.js';
 import '@bbva-web-components/bbva-web-form-text/bbva-web-form-text.js';
-
-import '@bbva-web-components/bbva-foundations-grid-default-layout/bbva-foundations-grid-default-layout.js';
+import { bbvaClose } from '@bbva-web-components/bbva-foundations-icons';
+import { BbvaCoreModal } from '@bbva-web-components/bbva-core-modal';
+import { BbvaCoreSlotsContentMixin } from '@bbva-web-components/bbva-core-slots-content-mixin';
+import { BbvaCoreFocusVisibleMixin } from '@bbva-web-components/bbva-core-focus-visible-mixin';
+import { BbvaCoreIntlMixin } from '@bbva-web-components/bbva-core-intl-mixin';
+import { BbvaCoreResizeObservableElementMixin } from '@bbva-web-components/bbva-core-resize-observable-element-mixin';
 
 import styles from './CellsIdentityDocumentFormModalPgc-styles.js';
 /**
@@ -24,6 +28,8 @@ Example:
 
 @customElement cells-identity-document-form-modal-pgc
 */
+const iconClose = bbvaClose();
+
 export class CellsIdentityDocumentFormModalPgc extends LitElement {
   static get is() {
     return 'cells-identity-document-form-modal-pgc';
@@ -41,12 +47,16 @@ export class CellsIdentityDocumentFormModalPgc extends LitElement {
       listDocumentsType: { type: Array },
       _documentType: { type: String },
       _documentNumber: { type: String },
+
+      closeIcon: { type: String,
+      attribute: 'close-label'}
     };
   }
 
   // Initialize properties
   constructor() {
     super();
+    this.closeIcon = iconClose;
     this.title = '';
     this.subtitle = '';
     this.documentTypeLabel = '';
@@ -59,23 +69,27 @@ export class CellsIdentityDocumentFormModalPgc extends LitElement {
   }
   async init(data) {
     this.reset();
-    this.title = data.title;
-    this.subtitle = data.subtitle;
-    this.documentTypeLabel = data.documentTypeLabel;
-    this.documentNumberLabel = data.documentNumberLabel;
-    this.buttonLabel = data.buttonLabel;
-    this.closeable = data.closeable;
-    this.listDocumentsType = data.listDocumentsType;
+    this.title = data.title ? data.title : this.title;
+    this.subtitle = data.subtitle ? data.subtitle : this.subtitle;
+    this.documentTypeLabel = data.documentTypeLabel
+      ? data.documentTypeLabel
+      : this.documentTypeLabel;
+    this.documentNumberLabel = data.documentNumberLabel
+      ? data.documentNumberLabel
+      : this.documentNumberLabel;
+    this.buttonLabel = data.buttonLabel ? data.buttonLabel : this.buttonLabel;
+    this.closeable = data.closeable ? data.closeable : this.closeable;
+    this.listDocumentsType = data.listDocumentsType
+      ? data.listDocumentsType
+      : this.listDocumentsType;
     this._documentType = '';
     this._documentNumber = '';
   }
   open() {
-    const modal = this.shadowRoot.querySelector('#modal');
-    modal.open();
+    this.open();
   }
   close() {
-    const modal = document.querySelector('#modal');
-    modal.closest();
+    this.close();
   }
   reset() {
     this.title = '';
@@ -95,7 +109,7 @@ export class CellsIdentityDocumentFormModalPgc extends LitElement {
       documentNumber: this._documentNumber,
     };
   }
-  _acceptClicket() {
+  _acceptClicked() {
     this.dispatchEvent(
       new CustomEvent('accept-clicked', {
         bubbles: true,
@@ -104,7 +118,7 @@ export class CellsIdentityDocumentFormModalPgc extends LitElement {
       })
     );
   }
-  _closeClicket() {
+  _closeClicked() {
     this.dispatchEvent(
       new CustomEvent('close-clicked', {
         bubbles: true,
@@ -125,35 +139,34 @@ export class CellsIdentityDocumentFormModalPgc extends LitElement {
   // Define a template
   render() {
     return html`
-      <bbva-web-template-modal id="modal" size="m" .close-clicked=${this._closeClicket}>
-        <label class="title">${this.title}</label>
-        <div class="body">
-          <label class="subtitle">${this.subtitle}</label>
-          <div class="g">
-            <div class="g-col-lg-6 g-col-md-6 g-col-sm-4 g-col-xs-2">
-              <slot name="left">
-              ${this.documentTypeLabel}
-              </slot>
-            </div>
-            <div class="g-col-lg-6 g-col-md-6 g-col-sm-4 g-col-xs-2">
-              <slot name="right">
-                <bbva-web-form-select></bbva-web-form-select>
-              </slot>
-            </div>
-            <div class="g-col-lg-6 g-col-md-6 g-col-sm-4 g-col-xs-2">
-              <slot name="left">
-              ${this.documentNumberLabel}
-              </slot>
-            </div>
-            <div class="g-col-lg-6 g-col-md-6 g-col-sm-4 g-col-xs-2">
-              <slot name="right">
-                <bbva-web-form-text></bbva-web-form-text>
-              </slot>
-            </div>
-          </div>
-          <bbva-web-button-default onclick=${this._acceptClicket()}>${this.buttonLabel}</bbva-web-button-default>
+      <div class="modal">
+        <div class="modal-header">
+          <label class="title">${this.title}</label>
+          <button type="button" class="close-btn" @click="${this._closeClicked}">
+            <bbva-core-icon class="close-icon" .icon="${this.closeIcon}"></bbva-core-icon>
+          </button>
         </div>
-      </bbva-web-template-modal>
+        <div class="modal-content">
+          <label class="subtitle">${this.subtitle}</label>
+          <div class="form-group">
+            <label> ${this.documentTypeLabel}:</label>
+            <bbva-web-form-select class="lg-6">
+              ${this.listDocumentsType.map(
+                (documentType) => html`
+                  <bbva-web-form-option>${documentType.type}</bbva-web-form-option>
+                `
+              )}
+            </bbva-web-form-select>
+          </div>
+          <div class="form-group">
+            <label> ${this.documentNumberLabel}:</label>
+            <bbva-web-form-text class="lg-6"></bbva-web-form-text>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <bbva-web-button-default @click=${this._acceptClicked}>${this.buttonLabel}</bbva-web-button-default>
+        </div>
+      </div>
     `;
   }
 }
